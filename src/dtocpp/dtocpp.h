0e4e1d9543222b93dfb1b387d5857706b10ae5a1
TimberWorm <github@s522411185.online.de>
2023-07-21 20:30:48 +0200

    added; Adding the base data types

--- src/dtocpp/dtocpp.h
+++ src/dtocpp/dtocpp.h
@@ -1,7 +1,21 @@
 #pragma once
 
+#include <cstdint>
+
 #define DTOCPP_NAMESPACE_BEGIN namespace dto {
 #define DTOCPP_NAMESPACE_END }
 
 
 
+using TInt8 = std::int8_t;
+using TInt16 = std::int16_t;
+using TInt32 = std::int32_t;
+using TInt64 = std::int64_t;
+
+using TUint8 = std::uint8_t;
+using TUint16 = std::uint16_t;
+using TUint32 = std::uint32_t;
+using TUint64 = std::uint64_t;
+
+using TFloat = float;
+using TDouble = double;
